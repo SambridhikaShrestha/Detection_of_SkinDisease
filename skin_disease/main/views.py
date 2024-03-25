84b7c81c9eb61fb3b3cec4c12d40826b0c9a0e40
sambridhika <shresthasambridhika009@gmail.com>
2024-03-25 21:51:20 +0545

    function for pre-process of the images

--- skin_disease/main/views.py
+++ skin_disease/main/views.py
@@ -9,4 +9,14 @@ from django.core.files.storage import FileSystemStorage
 model = keras.models.load_model('savedModel/model.h5')
 
 
+def convertIMG(testing_img):
+    img = Image.open(testing_img)
+    img_resized = img.resize((28, 28))
+    img_rgb = img_resized.convert('RGB')
+    img_array = np.array(img_rgb)
+    if img_array.shape[2] == 4:
+        img_array = img_array[:, :, :3]  
+    if img_array.shape[:2] != (28, 28):
+        raise ValueError("Image size is not as expected")
+    return img_array
 
