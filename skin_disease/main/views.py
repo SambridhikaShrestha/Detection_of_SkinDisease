b6db7672fceedd9b29fc5b8a7a7cb568facab63f
sambridhika <shresthasambridhika009@gmail.com>
2024-03-25 21:14:54 +0545

    Loaded the model

--- skin_disease/main/views.py
+++ skin_disease/main/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import keras
+from PIL import Image
+import numpy as np
+import os
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
-def index(request):
-    return render(request, "index.html")
+# Load the model
+model = keras.models.load_model('savedModel/model.h5')
+
+
+
