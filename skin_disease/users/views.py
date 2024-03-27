0a3e7faa9f16175129db23c412b0a4fe96c65e3f
sambridhika <shresthasambridhika009@gmail.com>
2024-03-27 22:36:03 +0545

    Function created for login

---
+++ skin_disease/users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, SignUpForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a success page.
+                return redirect('index')
+            else:
+                # Return an 'invalid login' error message.
+                return render(request, 'login.html', {'form': form, 'error_message': 'Invalid username or password.'})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
