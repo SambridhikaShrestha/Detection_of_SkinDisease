8cbc7a3d09c4677a5765e14e3ba0ab944be3f5a5
sambridhika <shresthasambridhika009@gmail.com>
2024-03-27 22:37:37 +0545

    Function created for signup

--- skin_disease/users/views.py
+++ skin_disease/users/views.py
@@ -19,3 +19,14 @@ def login_view(request):
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a success page.
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
