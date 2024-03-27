32a973894a939b1c058380113cfa32686beaa901
sambridhika <shresthasambridhika009@gmail.com>
2024-03-27 22:41:49 +0545

    Implemented signup forms

--- skin_disease/users/forms.py
+++ skin_disease/users/forms.py
@@ -5,3 +5,10 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
 
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'name', 'last_name', 'age']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
